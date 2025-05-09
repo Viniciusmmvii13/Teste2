ec25dbeca07d9f9501791ba733b0d8c1695cbb39
Suporte <vinicius.costammvii@gmail.com>
2025-05-09 13:52:56 -0300

    Vinicius

---
+++ Teste.py
@@ -0,0 +1,14 @@
+print('vini-san world')
+x=1
+while x <= 50:
+    print("*" * x)
+    x+=1
+while x >= 0:
+    print("*" * x)
+    x-=1
+y = 50
+while x <= 50:
+    print(" " * y +"*" * x + " " * y)
+    x+=1
+    y-=1
+#xeredelda
